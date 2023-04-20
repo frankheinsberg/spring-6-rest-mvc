088450d1c320fe2d2cea59036746e84ce117bb8f
frankheinsberg <130737090+frankheinsberg@users.noreply.github.com>
2023-04-20 07:35:50 +0200

    PatchMapping fuer Customer

--- src/main/java/guru/springframework/spring6restmvc/services/CustomerService.java
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerService.java
@@ -19,4 +19,6 @@ public interface CustomerService {
     void updateCustomerById(UUID id, Customer customer);
 
     void deleteById(UUID customerId);
+
+    void patchCustomerById(UUID customerId, Customer customer);
 }
